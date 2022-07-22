0f18bb894809fe9d1ab795f5a9828a202c2cb28d
Quantum0 <quantum0@mail.ru>
2022-07-22 18:04:28 +0300

    Create install_service.py

---
+++ install_service.py
@@ -0,0 +1,8 @@
+# TODO make this script
+
+# Check we are on linux and in opt dir
+# Get the project name from git repo name or ask for custom
+# Generate .service file
+# Make venv
+# Install
+# Print commands for status/stop/start
